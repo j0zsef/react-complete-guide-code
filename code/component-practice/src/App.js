466ccee720e14811f2aa4ff842b918e321861859
Joe Nevin <josephfnevin@gmail.com>
2023-11-06 16:33:11 -0600

    Adding header component.

--- code/component-practice/src/App.js
+++ code/component-practice/src/App.js
@@ -3,6 +3,7 @@ import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
 import Concepts from "./components/Concepts";
+import Header from "./components/Header";
 
 const concepts = [
   {
@@ -31,11 +32,7 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header/>
       <Concepts concepts={concepts} />
     </div>
   );
--- code/component-practice/src/components/Concepts.js
+++ code/component-practice/src/components/Concepts.js
@@ -3,11 +3,9 @@ import "./Concepts.css";
 
 function Concepts(props){
     return (
-        <div>
             <ul id="concepts">
                 {props.concepts.map((concept) => <Concept key={concept.id} image={concept.image} title={concept.title} description={concept.description} />)}
             </ul>
-        </div>
         )
 }
 
