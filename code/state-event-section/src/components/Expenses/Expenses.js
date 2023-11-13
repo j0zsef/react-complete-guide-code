94a80a7b0097bb1d62e897c90dc4300c727d6e3b
Joe Nevin <josephfnevin@gmail.com>
2023-11-13 10:13:20 -0600

    Adding ExpensesFilter and subsequent event handlers.

--- code/state-event-section/src/components/Expenses/Expenses.js
+++ code/state-event-section/src/components/Expenses/Expenses.js
@@ -1,17 +1,33 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 function Expenses(props) {
   const expenses = props.expenses.map((expense) => (
-    <ExpenseItem
-      key={expense.id}
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-    />
+      <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+      />
   ));
-  return <Card className="expenses">{expenses}</Card>;
+  const [filteredYear, setFilteredYear] = useState('2023')
+  const yearChangeHandler = function(newFilteredYear){
+    setFilteredYear(newFilteredYear)
+    console.log(newFilteredYear);
+  }
+  return (
+      <div>
+        <Card className="expenses">
+          <ExpensesFilter onYearChange={yearChangeHandler}/>
+          {expenses}
+        </Card>
+      </div>
+
+  );
+
 }
 
 export default Expenses;
